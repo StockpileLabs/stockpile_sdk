6e993c8778fc0fa738a4d660ce04c87e06740bac
bubu59 <3e2.weechongjingjoshua43@gmail.com>
2023-04-19 16:22:51 +0800

    chore; createUser function

--- src/user.ts
+++ src/user.ts
@@ -1,5 +1,12 @@
 import { SDK } from "./index";
 import * as anchor from "@project-serum/anchor"
+import { utf8 } from "@project-serum/anchor/dist/cjs/utils/bytes";
+import { PublicKey, SystemProgram } from "@solana/web3.js";
+import {
+    useAnchorWallet,
+    useConnection,
+    useWallet,
+  } from "@solana/wallet-adapter-react";
 
 export class User {
     private readonly sdk: SDK;
@@ -8,13 +15,27 @@ export class User {
         this.sdk = sdk;
     }
 
-    public async create(username: string, image: any) {
+    public async getProgramDerivedUserAddress (pk:any) {
+        const { program } = this.sdk;
+        const [userPDA, bump] = anchor.web3.PublicKey.findProgramAddressSync(
+            [utf8.encode("fuckItWeBall!"), pk.toBuffer() ],
+            program.programId
+        );
+        return {userPDA, bump}
+    }
+
+    public async create(username: string, image: any, owner:anchor.web3.PublicKey, pk:any) {
+        const { userPDA } = await this.getProgramDerivedUserAddress(pk)
         const instructionMethodBuilder = this.sdk.program.methods
           .createUser(String(username), String(image))
-        //   .accounts({
-        //     userAccount: userPDA,
-        //     authority: 
-        //   })
-        
+          .accounts({
+            userAccount: userPDA,
+            authority: owner,
+            systemProgram: SystemProgram.programId
+          })
+        return {
+            instructionMethodBuilder
+        }
     }
+
 }
